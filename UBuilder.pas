9c3e2b4ba81dba210c5d089980f3e44d0081257e
Guillaume Crégut <gcregut@free.fr>
2024-09-25 18:49:09 +0200

    [Style]: UBuilder : reformat document

--- UBuilder.pas
+++ UBuilder.pas
@@ -3,8 +3,10 @@ unit UBuilder;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, UdataModule, Data.DB, FireDAC.Stan.Intf,
+  System.UITypes,
   FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
   System.Actions, Vcl.ActnList, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
@@ -46,8 +48,8 @@ type
     procedure EBuilderUpdateNameChange(Sender: TObject);
   private
     { Déclarations privées }
-    oldName : string;
-    oldCountry : Integer;
+    oldName: string;
+    oldCountry: Integer;
     procedure reloadValues;
     procedure refreshValues;
   public
@@ -63,16 +65,16 @@ implementation
 
 procedure TFBuilder.ABuilberRemoveExecute(Sender: TObject);
 var
- IdBuilder : Integer;
- query, name : string;
+  IdBuilder: Integer;
+  query, name: string;
 begin
-     name := DSBuilder.DataSet.FieldByName('nom').AsString;
+  name := DSBuilder.DataSet.FieldByName('nom').AsString;
   if MessageDlg('Voulez vous supprimer ' + name + ' ?', TMsgDlgType.mtWarning,
     [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], 0, TMsgDlgBtn.mbNo) = mrYes then
   begin
-    idBuilder:=DSBuilder.DataSet.FieldByName('id').AsInteger;
-     query:= 'DELETE FROM builders WHERE id=:id';
-     DMDatabase.dbConnection.ExecSQL(query,[idBuilder]);
+    IdBuilder := DSBuilder.DataSet.FieldByName('id').AsInteger;
+    query := 'DELETE FROM builders WHERE id=:id';
+    DMDatabase.dbConnection.ExecSQL(query, [IdBuilder]);
     reloadValues;
     refreshValues;
   end;
@@ -80,99 +82,105 @@ end;
 
 procedure TFBuilder.BBuilderAddClick(Sender: TObject);
 var
-  idCountry : Integer;
-  name, query : String;
+  idCountry: Integer;
+  name, query: String;
 begin
- idCountry := DBLCBBuilderCountry.ListSource.DataSet.FieldByName('id').AsInteger;
- name := EBuilderNewName.Text;
- query := 'INSERT INTO builders (name, country) VALUES(:n,:c)';
+  idCountry := DBLCBBuilderCountry.ListSource.DataSet.FieldByName('id')
+    .AsInteger;
+  name := EBuilderNewName.Text;
+  query := 'INSERT INTO builders (name, country) VALUES(:n,:c)';
   try
-     DMDatabase.dbConnection.ExecSQL(query,[name,idCountry]);
+    DMDatabase.dbConnection.ExecSQL(query, [name, idCountry]);
   except
     showMessage('Le nom saisi existe déjà');
   end;
- reloadValues;
- refreshValues;
+  reloadValues;
+  refreshValues;
 end;
 
 procedure TFBuilder.BBuilderUpdateClick(Sender: TObject);
 var
-name : String ;
-idNewCountry : Integer;
+  name: String;
+  idNewCountry: Integer;
 begin
-   name:=EBuilderUpdateName.Text;
-   idNewCountry:= DBLCBBuilderUpdateCountry.ListSource.DataSet.FieldByName('id').AsInteger;
-   showMessage(IntToStr(idNewCountry));
+  name := EBuilderUpdateName.Text;
+  idNewCountry := DBLCBBuilderUpdateCountry.ListSource.DataSet.FieldByName('id')
+    .AsInteger;
+  showMessage(IntToStr(idNewCountry));
 end;
 
 procedure TFBuilder.DBGBuilderCellClick(Column: TColumn);
 begin
-    oldName:= DSBuilder.DataSet.FieldByName('nom').AsString;
-    oldCountry:=  DSBuilder.DataSet.FieldByName('country').AsInteger;
-    refreshValues;
+  oldName := DSBuilder.DataSet.FieldByName('nom').AsString;
+  oldCountry := DSBuilder.DataSet.FieldByName('country').AsInteger;
+  refreshValues;
 end;
 
 procedure TFBuilder.DBLCBBuilderCountryCloseUp(Sender: TObject);
 var
-  newName : string;
+  newName: string;
 begin
-    newName :=  EBuilderNewName.Text;
-    BBuilderAdd.Enabled:=(newName.Length>0) and (DBLCBBuilderCountry.Text.Length> 0) ;
+  newName := EBuilderNewName.Text;
+  BBuilderAdd.Enabled := (newName.Length > 0) and
+    (DBLCBBuilderCountry.Text.Length > 0);
 end;
 
 procedure TFBuilder.EBuilderNewNameChange(Sender: TObject);
 var
-  newName : string;
+  newName: string;
 begin
-     newName :=  EBuilderNewName.Text;
-     BBuilderAdd.Enabled:=(newName.Length>0) and (DBLCBBuilderCountry.Text.Length> 0) ;
+  newName := EBuilderNewName.Text;
+  BBuilderAdd.Enabled := (newName.Length > 0) and
+    (DBLCBBuilderCountry.Text.Length > 0);
 end;
 
 procedure TFBuilder.EBuilderNewNameExit(Sender: TObject);
 var
-  newName : string;
+  newName: string;
 begin
-    newName :=  EBuilderNewName.Text;
-    BBuilderAdd.Enabled:=(newName.Length>0) and (DBLCBBuilderCountry.Text.Length> 0) ;
+  newName := EBuilderNewName.Text;
+  BBuilderAdd.Enabled := (newName.Length > 0) and
+    (DBLCBBuilderCountry.Text.Length > 0);
 end;
 
 procedure TFBuilder.EBuilderUpdateNameChange(Sender: TObject);
 var
-  name : string;
+  name: string;
 begin
-    name := EBuilderUpdateName.Text;
-    BBuilderUpdate.Enabled:=name.Length>0;
+  name := EBuilderUpdateName.Text;
+  BBuilderUpdate.Enabled := name.Length > 0;
 end;
 
 procedure TFBuilder.FormShow(Sender: TObject);
 begin
-   if not FDQBuilderDisplay.Active then
-   begin
-      FDQBuilderDisplay.Active:=true;
-      DBGBuilder.DataSource.DataSet.First;
-      DBGBuilderCellClick(nil);
-   end;
+  if not FDQBuilderDisplay.Active then
+  begin
+    FDQBuilderDisplay.Active := true;
+    DBGBuilder.DataSource.DataSet.First;
+    DBGBuilderCellClick(nil);
+  end;
 
-   if not DMDatabase.FDTableCountry.Active then
-     DMDatabase.FDTableCountry.Active:=true;
-   DMDatabase.FDTableCountry.First;
-   DBLCBBuilderCountry.ListSource.DataSet.First;
-   if DBLCBBuilderCountry.Text.Length= 0 then
-       BBuilderAdd.Enabled:=false;
-   refreshValues;
+  if not DMDatabase.FDTableCountry.Active then
+    DMDatabase.FDTableCountry.Active := true;
+  DMDatabase.FDTableCountry.First;
+  DBLCBBuilderCountry.ListSource.DataSet.First;
+  if DBLCBBuilderCountry.Text.Length = 0 then
+    BBuilderAdd.Enabled := false;
+  refreshValues;
 end;
 
 procedure TFBuilder.refreshValues;
 begin
-  EBuilderUpdateName.Text := DBGBuilder.DataSource.DataSet.FieldByName('nom').AsString;
+  EBuilderUpdateName.Text := DBGBuilder.DataSource.DataSet.FieldByName
+    ('nom').AsString;
 end;
 
 procedure TFBuilder.reloadValues;
 begin
-   if FDQBuilderDisplay.Active then
-    FDQBuilderDisplay.Active:=false;
-    //Do some stuff here
-    FDQBuilderDisplay.Active:=true;
+  if FDQBuilderDisplay.Active then
+    FDQBuilderDisplay.Active := false;
+  // Do some stuff here
+  FDQBuilderDisplay.Active := true;
 end;
 
 end.
