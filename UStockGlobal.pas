1f99935d3292cde7050c03f4a24e0c1fe9e56591
Guillaume Crégut <gcregut@free.fr>
2024-09-28 12:08:36 +0200

    [Feat]: Create UStockGlobal

---
+++ UStockGlobal.pas
@@ -0,0 +1,24 @@
+unit UStockGlobal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFStockGlobal = class(TForm)
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  FStockGlobal: TFStockGlobal;
+
+implementation
+
+{$R *.dfm}
+
+end.
