5265579056e692109ca0194f0f2096aa1feaa3bc
Guillaume Crégut <gcregut@free.fr>
2024-09-30 22:17:33 +0200

    [Feat]: UFinishedKit: create page

---
+++ UFinishedKit.dfm
@@ -0,0 +1,14 @@
+object FFinishedKit: TFFinishedKit
+  Left = 0
+  Top = 0
+  Caption = 'Kits termin'#233's'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ UFinishedKit.pas
@@ -0,0 +1,24 @@
+unit UFinishedKit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFFinishedKit = class(TForm)
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  FFinishedKit: TFFinishedKit;
+
+implementation
+
+{$R *.dfm}
+
+end.
