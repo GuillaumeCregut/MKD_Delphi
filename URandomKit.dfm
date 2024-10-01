1d54ecaa01121ec82c24b5bb08cc60c493cc0e9a
Guillaume Crégut <gcregut@free.fr>
2024-10-01 18:34:50 +0200

    [Feat]: URandomKit: create page

--- URandomKit.dfm
+++ URandomKit.dfm
@@ -2,13 +2,154 @@ object FRandomKit: TFRandomKit
   Left = 0
   Top = 0
   Caption = 'FRandomKit'
-  ClientHeight = 441
-  ClientWidth = 624
+  ClientHeight = 329
+  ClientWidth = 602
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  OnShow = FormShow
   TextHeight = 15
+  object Label1: TLabel
+    Left = 328
+    Top = 16
+    Width = 33
+    Height = 15
+    Caption = 'Nom :'
+  end
+  object LRandomName: TLabel
+    Left = 376
+    Top = 16
+    Width = 83
+    Height = 15
+    Caption = 'LRandomName'
+  end
+  object Label3: TLabel
+    Left = 328
+    Top = 48
+    Width = 47
+    Height = 15
+    Caption = 'Marque :'
+  end
+  object LRandomBrand: TLabel
+    Left = 381
+    Top = 48
+    Width = 82
+    Height = 15
+    Caption = 'LRandomBrand'
+  end
+  object Label5: TLabel
+    Left = 328
+    Top = 110
+    Width = 75
+    Height = 15
+    Caption = 'Constructeur :'
+  end
+  object LRandomBuilder: TLabel
+    Left = 409
+    Top = 110
+    Width = 88
+    Height = 15
+    Caption = 'LRandomBuilder'
+  end
+  object Label7: TLabel
+    Left = 328
+    Top = 136
+    Width = 43
+    Height = 15
+    Caption = 'Echelle :'
+  end
+  object LRandomReference: TLabel
+    Left = 392
+    Top = 80
+    Width = 103
+    Height = 15
+    Caption = 'LRandomReference'
+  end
+  object Label9: TLabel
+    Left = 328
+    Top = 168
+    Width = 46
+    Height = 15
+    Caption = 'P'#233'riode :'
+  end
+  object LRandomScale: TLabel
+    Left = 377
+    Top = 136
+    Width = 78
+    Height = 15
+    Caption = 'LRandomScale'
+  end
+  object Label11: TLabel
+    Left = 329
+    Top = 200
+    Width = 57
+    Height = 15
+    Caption = 'Cat'#233'gorie :'
+  end
+  object LRandomCategory: TLabel
+    Left = 392
+    Top = 200
+    Width = 99
+    Height = 15
+    Caption = 'LRandomCategory'
+  end
+  object Label13: TLabel
+    Left = 328
+    Top = 80
+    Width = 58
+    Height = 15
+    Caption = 'R'#233'f'#233'rence :'
+  end
+  object LRandomPeriod: TLabel
+    Left = 380
+    Top = 168
+    Width = 85
+    Height = 15
+    Caption = 'LRandomPeriod'
+  end
+  object Label15: TLabel
+    Left = 329
+    Top = 232
+    Width = 65
+    Height = 15
+    Caption = 'Scalemates :'
+  end
+  object IRandomKit: TImage
+    Left = 8
+    Top = 8
+    Width = 297
+    Height = 225
+    Proportional = True
+    Stretch = True
+  end
+  object BRandomMoveKit: TButton
+    Left = 24
+    Top = 272
+    Width = 137
+    Height = 25
+    Caption = 'D'#233'placer sur l'#39'atelier'
+    TabOrder = 0
+    OnClick = BRandomMoveKitClick
+  end
+  object LLRandomScalemates: TLinkLabel
+    Left = 400
+    Top = 232
+    Width = 120
+    Height = 19
+    Caption = 'LLRandomScalemates'
+    TabOrder = 1
+    OnLinkClick = LLRandomScalematesLinkClick
+  end
+  object BRandomOtherKit: TButton
+    Left = 224
+    Top = 272
+    Width = 151
+    Height = 25
+    Caption = 'Proposer un autre kit'
+    TabOrder = 2
+    OnClick = BRandomOtherKitClick
+  end
 end
--- URandomKit.pas
+++ URandomKit.pas
@@ -3,15 +3,45 @@ unit URandomKit;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, System.UITypes, Vcl.Forms, Vcl.Dialogs, UdataModule,
+  Vcl.ExtCtrls, Vcl.StdCtrls, Winapi.ShellAPI;
 
 type
   TFRandomKit = class(TForm)
+    Label1: TLabel;
+    LRandomName: TLabel;
+    Label3: TLabel;
+    LRandomBrand: TLabel;
+    Label5: TLabel;
+    LRandomBuilder: TLabel;
+    Label7: TLabel;
+    LRandomReference: TLabel;
+    Label9: TLabel;
+    LRandomScale: TLabel;
+    Label11: TLabel;
+    LRandomCategory: TLabel;
+    Label13: TLabel;
+    LRandomPeriod: TLabel;
+    Label15: TLabel;
+    IRandomKit: TImage;
+    BRandomMoveKit: TButton;
+    LLRandomScalemates: TLinkLabel;
+    BRandomOtherKit: TButton;
+    procedure FormShow(Sender: TObject);
+    procedure BRandomOtherKitClick(Sender: TObject);
+    procedure LLRandomScalematesLinkClick(Sender: TObject; const Link: string;
+      LinkType: TSysLinkType);
+    procedure BRandomMoveKitClick(Sender: TObject);
   private
     { Déclarations privées }
+     procedure LoadPicture(name : String);
+     procedure randomKitShow();
+     procedure ShellOpen(Url: string; Params: string = '');
   public
     { Déclarations publiques }
+    idKit: Integer;
   end;
 
 var
@@ -20,5 +50,108 @@ var
 implementation
 
 {$R *.dfm}
+uses
+  Vcl.Imaging.pngimage,Vcl.Imaging.jpeg, System.IOUtils;
 
+procedure TFRandomKit.BRandomMoveKitClick(Sender: TObject);
+var
+  name : String;
+begin
+  name:= LRandomName.Caption;
+  if MessageDlg('Voulez vous déplacer ' + name + ' dans les montages en cours ?', TMsgDlgType.mtWarning,
+    [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], 0, TMsgDlgBtn.mbNo) = mrYes then
+  begin
+     DMDatabase.moveStock(idKit,DMDatabase.onStash);
+     Close;
+  end;
+end;
+
+procedure TFRandomKit.BRandomOtherKitClick(Sender: TObject);
+var
+  query : String;
+begin
+  DMDatabase.SystemQuery.Close;
+  query:='SELECT id FROM model_user WHERE state=:status  ORDER BY RANDOM() LIMIT  1;';
+  DMDatabase.SystemQuery.Open(query,[DMDatabase.inStock]);
+  idKit:=DMDatabase.DSSystem.DataSet.FieldByName('id').AsInteger;
+  DMDatabase.SystemQuery.Close;
+  randomKitShow;
+end;
+
+procedure TFRandomKit.FormShow(Sender: TObject);
+
+begin
+  randomKitShow;
+end;
+
+procedure TFRandomKit.randomKitShow();
+var
+  query, picture, scalemates: String;
+begin
+  query := '''
+    SELECT mu.id, m.name, m.reference as reference, m.picture as picture,
+    m.scalemates as scalemates, b.name as buildername, br.name as brandname,
+    s.name as scalename, p.name as period,
+    c.name as category FROM model_user mu INNER JOIN model m ON mu.model_id = m.
+    id INNER JOIN builders b ON m.builder = b.id INNER JOIN brand br ON m.
+    brand = br.id INNER JOIN scale s ON m.scale = s.id INNER JOIN period p ON m.
+    period = p.id INNER JOIN category c ON m.category = c.id WHERE mu.id =:id
+  ''';
+  DMDatabase.SystemQuery.Open(query,[idKit]);
+  scalemates:=DMDatabase.DSSystem.DataSet.FieldByName('scalemates').asString;
+  picture:= DMDatabase.DSSystem.DataSet.FieldByName('picture').asString;
+  LRandomName.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('name').asString;
+  LRandomBuilder.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('buildername').asString;
+  LRandomPeriod.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('period').asString;
+  LRandomScale.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('scalename').asString;
+  LRandomBrand.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('brandname').asString;
+  LRandomCategory.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('category').asString;
+  LRandomReference.Caption:=DMDatabase.DSSystem.DataSet.FieldByName('reference').asString;
+  DMDatabase.SystemQuery.Close;
+  if scalemates.Length > 0 then
+    LLRandomScalemates.Caption := '<a href="' + scalemates + '">Lien scalemates</a>'
+  else
+    LLRandomScalemates.caption := '-';
+  LoadPicture(picture);
+end;
+
+procedure TFRandomKit.LLRandomScalematesLinkClick(Sender: TObject;
+  const Link: string; LinkType: TSysLinkType);
+begin
+  if LLRandomScalemates.Caption<>'-' then
+  ShellOpen(Link);
+end;
+
+procedure TFRandomKit.LoadPicture(name: String);
+var
+  picturePath: string;
+  RS: TResourceStream;
+  Image: TPngImage;
+begin
+picturePath := TPath.Combine(ExtractFileDir(Application.ExeName),'pictures',name);
+  if not FileExists(picturePath) then
+  begin
+    Image := TPngImage.Create;
+    try
+      RS := TResourceStream.Create(hInstance, 'No_image', RT_RCDATA);
+      try
+        Image.LoadFromStream(RS);
+        IRandomKit.picture.Graphic := Image;
+      finally
+        RS.Free;
+      end;
+    finally
+      Image.Free;
+    end;
+  end
+  else
+  begin
+    IRandomKit.Picture.LoadFromFile(picturePath);
+  end;
+end;
+
+procedure TFRandomKit.ShellOpen(Url, Params: string);
+begin
+  ShellExecute(0, 'Open', pChar(Url), pChar(Params), nil, SW_SHOWNORMAL);
+end;
 end.
