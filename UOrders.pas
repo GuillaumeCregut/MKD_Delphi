c0f8d8cad602369a74acd5f0ea6be7bbc439a421
Guillaume Crégut <gcregut@free.fr>
2024-10-02 15:28:04 +0200

    [Feat]: UOrders: create page

---
+++ UOrders.pas
@@ -0,0 +1,24 @@
+unit UOrders;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFOrders = class(TForm)
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  FOrders: TFOrders;
+
+implementation
+
+{$R *.dfm}
+
+end.
